b139d1fea4e0b7314b307295e7659e00239b8d8a
Quan Ming Vang <mingvang2003@gmail.com>
2025-01-06 11:13:46 +0000

    Completed exercises 1-3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let test = '';
+    for(let i = 1; i <= word.length; i++){
+        test = test + word[word.length-i];
+    }
+    return test;
 };
 
 // Do not edit below this line
