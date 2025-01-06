d712a074f2a72cd832e6f9fd33b4bd0310dbf236
Quan Ming Vang <mingvang2003@gmail.com>
2025-01-06 11:15:43 +0000

    Completed solutions for 1-3 javascript.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const adder = string;
+    if(num === 0){
+        return '';
+    }
+    else if(num < 0)
+    {
+        return "ERROR";
+    }
+    for(let i = 0; i < num-1; i++){
+        string = string.concat(adder);
+    }
+    return string;
 };
 
 // Do not edit below this line
